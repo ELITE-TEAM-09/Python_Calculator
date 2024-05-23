be65ea344d25adf228791a320d14fbdeac935499
Collins Gichuki <gichukimuchiri80@gmail.com>
2024-05-23 14:42:19 +0300

    Optimized App

--- calculator.py
+++ calculator.py
@@ -1,31 +1,17 @@
-# Function to add two numbers
-def add(x, y):
-    return x + y
-
-# Function to subtract the second number from the first
-def subtract(x, y):
-    return x - y
-
-# Function to multiply two numbers
-def multiply(x, y):
-    return x * y
-
-# Function to divide the first number by the second
-def divide(x, y):
-    if y == 0:
-        return "Error! Division by zero."  # Handle division by zero
-    else:
-        return x / y
-
-# Main calculator function
+# calculator function
 def calculator():
-    print("Select operation:")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
+    def print_operations():
+        print("Select operation:")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
 
     while True:
+
+         # Print the available operations
+        print_operations()
+
         # Prompt the user to select an operation
         choice = input("Enter choice(1/2/3/4): ")
 
@@ -42,13 +28,14 @@ def calculator():
 
             # Perform the chosen operation and print the result
             if choice == '1':
-                print(f"{num1} + {num2} = {add(num1, num2)}")
+                print(f"{num1} + {num2} = {num1 + num2}")
             elif choice == '2':
-                print(f"{num1} - {num2} = {subtract(num1, num2)}")
+                print(f"{num1} - {num2} = {num1 - num2}")
             elif choice == '3':
-                print(f"{num1} * {num2} = {multiply(num1, num2)}")
+                print(f"{num1} * {num2} = {num1 * num2}")
             elif choice == '4':
-                print(f"{num1} / {num2} = {divide(num1, num2)}")
+                result = "Error! Division by zero." if num2 == 0 else num1 / num2
+                print(f"{num1} / {num2} = {result}")
         else:
             # Handle invalid operation choice
             print("Invalid input. Please enter a number between 1 and 4.")
